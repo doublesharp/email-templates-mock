66232f388d6bbfab9b03c6dad95eae9f817416ec
Justin Silver <jsilver@doublesharp.com>
2016-09-20 13:42:36 -0700

    Implement AirBnb styleguide Switch from JSHint to ESLint

--- index.js
+++ index.js
@@ -1,3 +1,1 @@
-'use strict'
-
-module.exports = require('./lib/email-templates-mock')
+module.exports = require('./lib/email-templates-mock');
--- test/email-templates-mock-tests.js
+++ test/email-templates-mock-tests.js
@@ -1,194 +1,124 @@
-'use strict'
+const should = require('should');
+const path = require('path');
+const messages = require('../lib/messages');
+const emailtemplatesMock = require('../');
 
-const should = require('should')
-const fs = require('fs')
-const path = require('path')
-const messages = require('../lib/messages')
-const emailtemplatesMock = require('../')
-const templateDir = path.join(__dirname, './templates/example')
-const templateMock = new emailtemplatesMock.EmailTemplate(templateDir)
-const badDir = path.join(__dirname, './templates/bad-path')
-const badMock = new emailtemplatesMock.EmailTemplate(badDir)
+const templateDir = path.join(__dirname, './templates/example');
+const badDir = path.join(__dirname, './templates/bad-path');
 
-describe('Testing email-templates-mock...', function(){
+const templateMock = new emailtemplatesMock.EmailTemplate(templateDir);
+const badMock = new emailtemplatesMock.EmailTemplate(badDir);
 
+describe('Testing email-templates-mock...', () => {
   const inputs = {
-    subject: 'Mocked subject', 
+    subject: 'Mocked subject',
     html: 'html & encode',
-    text: 'plain text'
-  }
+    text: 'plain text',
+  };
 
-  const htmlEncoded = '<strong>html &amp; encode</strong>'
+  const htmlEncoded = '<strong>html &amp; encode</strong>';
 
-  beforeEach(function(done){
-
+  beforeEach((done) => {
     // Reset the mock to default values after each test
-    emailtemplatesMock.mock.reset()
+    emailtemplatesMock.mock.reset();
+    return done();
+  });
 
-    done()
-
-  })
-
-  it('should succeed to render the email using the real email-templates', function(done){
-
-    templateMock.render(inputs, function(err, email){
-      if (err){
-        return done(err)
+  it('should succeed to render the email using the real email-templates', (done) => {
+    templateMock.render(inputs, (err, email) => {
+      if (err) {
+        return done(err);
       }
+      email.html.should.be.exactly(htmlEncoded);
+      return done();
+    });
+  });
 
-      email.html.should.be.exactly(htmlEncoded)
+  it('should succeed to render the email using the mocked callback', (done) => {
+    emailtemplatesMock.mock.mockedRender(true);
+    templateMock.render(inputs, (err, email) => {
+      should(err).be.exactly(null);
+      email.html.should.be.exactly(messages.email.html);
+      return done();
+    });
+  });
 
-      done()
+  it('should fail to render the email using the mocked callback', (done) => {
+    emailtemplatesMock.mock.shouldFailOnce();
+    templateMock.render(inputs, (firstErr) => {
+      should(firstErr).not.be.exactly(null);
+      firstErr.should.be.exactly(messages.fail_response);
+      templateMock.render(inputs, (secondErr, email) => {
+        should(secondErr).be.exactly(null);
+        email.html.should.be.exactly(htmlEncoded);
+        return done();
+      });
+    });
+  });
 
-    })
+  it(`should fail to render the email twice using the mocked callback 
+    then succeed using the real nodemailer`, (done) => {
+    emailtemplatesMock.mock.shouldFail(true);
+    templateMock.render(inputs, (firstErr) => {
+      should(firstErr).not.be.exactly(null);
+      firstErr.should.be.exactly(messages.fail_response);
+      templateMock.render(inputs, (secondErr) => {
+        should(secondErr).not.be.exactly(null);
+        secondErr.should.be.exactly(messages.fail_response);
+        emailtemplatesMock.mock.shouldFail(false);
+        templateMock.render(inputs, (thirdErr, email) => {
+          should(thirdErr).be.exactly(null);
+          email.html.should.be.exactly(htmlEncoded);
+          return done();
+        });
+      });
+    });
+  });
 
-  })
+  it('should fail to render the email for a bad path using the mocked callback', (done) => {
+    badMock.render(inputs, (err) => {
+      should(err).be.exactly(messages.fail_response);
+      return done();
+    });
+  });
 
-  it('should succeed to render the email using the mocked callback', function(done){
+  it(`should fail to render the email for a bad path using the 
+    real nodemailer with validation off`, (done) => {
+    emailtemplatesMock.mock.validateTemplateDir(false);
+    badMock.render(inputs, (err) => {
+      should(err).not.be.exactly(null);
+      err.code.should.be.exactly('ENOENT');
+      return done();
+    });
+  });
 
-    emailtemplatesMock.mock.mockedRender(true)
-
-    templateMock.render(inputs, function(err, email){
-
-      should(err).be.exactly(null)
-
-      email.html.should.be.exactly(messages.email.html)
-
-      done()
-
-    })
-
-  })
-
-  it('should fail to render the email using the mocked callback', function(done){
-
-    emailtemplatesMock.mock.shouldFailOnce()
-
-    templateMock.render(inputs, function(err, email){
-
-      should(err).not.be.exactly(null)
-      err.should.be.exactly(messages.fail_response)
-
-      templateMock.render(inputs, function(err, email){
-
-        should(err).be.exactly(null)
-
-        email.html.should.be.exactly(htmlEncoded)
-
-        done()
-
-      })
-
-    })
-
-  })
-
-  it('should fail to render the email twice using the mocked callback then succeed using the real nodemailer', function(done){
-
-    emailtemplatesMock.mock.shouldFail(true)
-
-    templateMock.render(inputs, function(err, email){
-
-      should(err).not.be.exactly(null)
-
-      err.should.be.exactly(messages.fail_response)
-
-      templateMock.render(inputs, function(err, email){
-
-        should(err).not.be.exactly(null)
-
-        err.should.be.exactly(messages.fail_response)
-
-        emailtemplatesMock.mock.shouldFail(false)
-
-        templateMock.render(inputs, function(err, email){
-
-          should(err).be.exactly(null)
-
-          email.html.should.be.exactly(htmlEncoded)
-
-          done()
-
-        })
-
-      })
-
-    })
-
-  })
-
-  it('should fail to render the email for a bad path using the mocked callback', function(done){
-
-    badMock.render(inputs, function(err, email){
-      
-      should(err).be.exactly(messages.fail_response)
-
-      done()
-
-    })
-
-  })
-
-  it('should fail to render the email for a bad path using the real nodemailer with validation off', function(done){
-
-    emailtemplatesMock.mock.validateTemplateDir(false)
-
-    badMock.render(inputs, function(err, email){
-      
-      should(err).not.be.exactly(null)
-
-      err.code.should.be.exactly('ENOENT')
-
-      done()
-
-    })
-
-  })
-
-  it('should return a custom email response in the mocked callback', function(done){
-
+  it('should return a custom email response in the mocked callback', (done) => {
     const customEmail = {
-      subject: 'custom subject', 
+      subject: 'custom subject',
       html: 'custom html &amp; encode',
-      text: 'custom plain text'
-    }
+      text: 'custom plain text',
+    };
+    emailtemplatesMock.mock.email(customEmail);
+    emailtemplatesMock.mock.mockedRender(true);
+    templateMock.render(inputs, (err, email) => {
+      should(err).be.exactly(null);
+      email.html.should.be.exactly(customEmail.html);
+      return done();
+    });
+  });
 
-    emailtemplatesMock.mock.email(customEmail)
-    emailtemplatesMock.mock.mockedRender(true)
-
-    templateMock.render(inputs, function(err, email){
-      
-      should(err).be.exactly(null)
-
-      email.html.should.be.exactly(customEmail.html)
-
-      done()
-
-    })
-
-  })
-
-  it('should return a custom error response in the mocked callback', function(done){
-
+  it('should return a custom error response in the mocked callback', (done) => {
     const customError = {
       errno: 12345,
       code: 'Custom code',
-      message: 'Custom message'
-    }
-
-    emailtemplatesMock.mock.failResponse(customError)
-    emailtemplatesMock.mock.shouldFailOnce()
-
-    templateMock.render(inputs, function(err, email){
-      
-      should(err).not.be.exactly(null)
-
-      err.message.should.be.exactly(customError.message)
-
-      done()
-
-    })
-
-  })
-
-})
+      message: 'Custom message',
+    };
+    emailtemplatesMock.mock.failResponse(customError);
+    emailtemplatesMock.mock.shouldFailOnce();
+    templateMock.render(inputs, (err) => {
+      should(err).not.be.exactly(null);
+      err.message.should.be.exactly(customError.message);
+      return done();
+    });
+  });
+});
